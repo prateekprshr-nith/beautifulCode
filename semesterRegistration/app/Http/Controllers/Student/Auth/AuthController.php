61de1291d62f81b410ef0bb3027ca16bb33cfd99
Prateek Prasher <prateekprshr@gmail.com>
2016-02-25 22:29:54 +0530

    Added code for student auth
    In this commit, I have added various variables, that override the default variables, so that I can show custom login/registration and other views for student auth

--- semesterRegistration/app/Http/Controllers/Student/Auth/AuthController.php
+++ semesterRegistration/app/Http/Controllers/Student/Auth/AuthController.php
@@ -1,34 +1,37 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Student\Auth;
 
-use App\User;
+use App\Student;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
+/**
+ * Class AuthController, this class handles
+ * the login and registration of a student
+ *
+ * @package App\Http\Controllers\Student\Auth
+ */
 class AuthController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Registration & Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
-    |
-    */
-
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-    /**
-     * Where to redirect users after login / registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
+    // Authentication guard to be used
+    protected $guard = 'student';
+
+    // Redirect to this url after login/registration
+    protected $redirectTo = '/students/home';
+
+    // Registration view
+    protected $registerView = 'student.auth.register';
+
+    // Login view
+    protected $loginView = 'student.auth.login';
+
+    // User name field for login
+    protected $username = 'rollNo';
 
     /**
      * Create a new authentication controller instance.
@@ -49,24 +52,46 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
+            'rollNo' => 'required|regex:/[0-9]{2}M?[0-9]{3}/|unique:students',
+            'dCode' => 'required|regex:/[A-Z]{3,5}/',
+            'semNo' => 'required|regex:/[0-9]{1,2}/',
+            'registrationNo' => 'required|regex:/[0-9]+\/[0-9]+/|unique:students',
+            'sectionId' => 'required|regex:/[A-Z][123]/',
             'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|confirmed|min:6',
+            'fatherName' => 'required|max:255',
+            'motherName' =>'required|max:255',
+            'email' => 'required|email|max:255|unique:students',
+            'phoneNo' => 'required|regex:/(\+91)?[0-9]{10}/|unique:students',
+            'currentAddress' => 'required',
+            'permanentAddress' => 'required',
+            'password' => 'required|min:8',
+            'dob' => 'required|date',
         ]);
     }
 
     /**
-     * Create a new user instance after a valid registration.
+     * Create a new Student instance after a valid registration.
      *
      * @param  array  $data
-     * @return User
+     * @return Student
      */
     protected function create(array $data)
     {
-        return User::create([
+        return Student::create([
+            'rollNo' => $data['rollNo'],
+            'dCode' => $data['dCode'],
+            'semNo' => $data['semNo'],
+            'registrationNo' => $data['registrationNo'],
+            'sectionId' => $data['sectionId'],
             'name' => $data['name'],
+            'fatherName' => $data['fatherName'],
+            'motherName' => $data['motherName'],
             'email' => $data['email'],
+            'phoneNo' => $data['phoneNo'],
+            'currentAddress' => $data['currentAddress'],
+            'permanentAddress' => $data['permanentAddress'],
             'password' => bcrypt($data['password']),
+            'dob' => $data['dob'],
         ]);
     }
 }
