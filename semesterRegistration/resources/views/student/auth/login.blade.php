46c9b1c3b1a18152a347a62d56980371f0ca762e
Prateek Prasher <prateekprshr@gmail.com>
2016-02-25 23:31:01 +0530

    Added student login view

--- semesterRegistration/resources/views/student/auth/login.blade.php
+++ semesterRegistration/resources/views/student/auth/login.blade.php
@@ -1,45 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {!! csrf_field() !!}
+    <div class="container col-sm-offset-2 col-sm-8">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Login: Please enter your details correctly
+            </div>
+            <div class="panel-body">
+                <form method="POST" action="/students/login" accept-charset="UTF-8" id="loginForm">
+                    <input required name="_token" type="hidden">
+                    {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
+                    <!-- Display Validation Errors -->
+                    @include('common.errors')
 
+                    <!-- First row Roll no-->
+                    <div class="row">
+                        <div class="form-group row col-md-12 center-block">
+                            <div class="col-md-3 text-right">
+                                <label for="rollNo">Roll No</label>
+                            </div>
                             <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                                <input required class="form-control" name="rollNo" type="text" id="rollNo">
                             </div>
                         </div>
+                    </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
-
+                    <!-- Second row password-->
+                    <div class="row">
+                        <div class="form-group row col-md-12 center-block">
+                            <div class="col-md-3 text-right ">
+                                <label for="password">Password</label>
+                            </div>
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                                <input required class="form-control" name="password" type="password" id="password">
                             </div>
                         </div>
+                    </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                    <!-- Third row Login-->
+                    <div class="row">
+                        <div class="form-group row col-md-12 center-block">
+                            <div class="col-md-3">
+                            </div>
+                            <div class="form-group col-md-offset-3 col-md-3 center-block">
+                                <button class="btn btn-default" type="submit" id="loginButton">
+                                    <span class="glyphicon glyphicon-log-in"></span> Login
+                                </button>
+                            </div>
+                            <div class="form-group col-md-offset-3 col-md-3 center-block">
                                 <div class="checkbox">
                                     <label>
                                         <input type="checkbox" name="remember"> Remember Me
@@ -47,20 +56,9 @@
                                 </div>
                             </div>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i>Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</div>
 @endsection
