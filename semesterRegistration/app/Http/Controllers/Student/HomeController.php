ce544e3a4ff3879641a7c2dd7eb5ebed89346c6a
Prateek Prasher <prateekprshr@gmail.com>
2016-02-26 00:56:56 +0530

    Added student home controller

---
+++ semesterRegistration/app/Http/Controllers/Student/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Student;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
+/**
+ * Class HomeController, this class contains
+ * all the methods for student tasks like
+ * semester registration, profile update
+ * and all others that are left
+ * 
+ * @package App\Http\Controllers\Student
+ */
+class HomeController extends \App\Http\Controllers\Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:student');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('student.home');
+    }
+}
