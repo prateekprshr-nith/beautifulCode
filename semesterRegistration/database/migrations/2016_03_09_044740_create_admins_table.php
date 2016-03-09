e5687c83c0c7fa08236289254af74007e15784ee
Prateek Prasher <prateekprshr@gmail.com>
2016-03-09 10:37:28 +0530

    Added admins table

---
+++ semesterRegistration/database/migrations/2016_03_09_044740_create_admins_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateAdminsTable, this migration
+ * creates the 'admins' table in database
+ */
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table)
+        {
+            $table->string('adminId');
+            $table->string('password', '100');
+            $table->timestamps();
+
+            // Key constraints
+            $table->primary('adminId');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('admins');
+    }
+}
