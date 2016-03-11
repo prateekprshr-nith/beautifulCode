8701f8fbdc0ee25740fcf1ac6aaaa8bdf2bcec32
Prateek Prasher <prateekprshr@gmail.com>
2016-03-12 01:45:51 +0530

    Added RedirectToHome middleware
    This middleware handles the redirection of the authenticated users when they try to visit the landing page of the application

---
+++ semesterRegistration/app/Http/Middleware/RedirectToHome.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Class RedirectToHome, this class handles
+ * the redirection of authenticated users
+ * in case they try to visit the landing
+ * page of the application.
+ *
+ * @package App\Http\Middleware
+ */
+class RedirectToHome
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $guardArr = [
+            'student', 'teacher', 'libraryStaff',
+            'hostelStaff', 'adminStaff', 'admin',
+        ];
+
+        // Check if any of the above
+        // user is logged in and
+        // redirect accordingly
+        foreach($guardArr as $guard)
+        {
+            if (Auth::guard($guard)->check())
+            {
+                $redirectTo = '/' . $guard .'s/home';
+                return redirect($redirectTo);
+            }
+        }
+
+        return $next($request);
+    }
+}
