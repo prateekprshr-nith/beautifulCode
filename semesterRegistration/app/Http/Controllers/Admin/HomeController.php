fd1f4f969ca307b8f996e2cecf41a5553494bdca
Prateek Prasher <prateekprshr@gmail.com>
2016-03-12 22:45:41 +0530

    Added user management functions
    The functions show{teacher, {library, hostel, admin}staff}RegisForm were renamed to manage{teacher, {library, hostel, admin}staff}. Functions remove{teacher, {library, hostel, admin}staff} were also added to remove the specified users

--- semesterRegistration/app/Http/Controllers/Admin/HomeController.php
+++ semesterRegistration/app/Http/Controllers/Admin/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Teacher;
+use App\Student;
+use App\AdminStaff;
+use App\HostelStaff;
+use App\LibraryStaff;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -38,41 +43,141 @@ class HomeController extends Controller
 
     /**
      * Show the teacher registration form
+     * and currently registered teachers
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function manageTeachers()
     {
-        return view('teacher.auth.register');
+        $teacherArr = Teacher::all();
+        return view('teacher.auth.register', ['teachers' => $teacherArr, 'count' => 0]);
+    }
+
+    /**
+     * Remove a teacher
+     *
+     * @param $id
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
+    public function removeTeacher($id)
+    {
+        if ($id != null)
+        {
+            Teacher::destroy($id);
+        }
+
+        return redirect('admins/manage/teachers');
     }
 
     /**
      * Show the libraryStaff registration form
+     * and currently registered libraryStaff
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function manageLibraryStaff()
+    public function manageLibraryStaffs()
     {
-        return view('libraryStaff.auth.register');
+        $libraryStaffArray = LibraryStaff::all();
+        return view('libraryStaff.auth.register', ['libraryStaffs' => $libraryStaffArray, 'count' => 0]);
+    }
+
+    /**
+     * Remove a libraryStaff
+     *
+     * @param $id
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
+    public function removeLibraryStaff($id)
+    {
+        if ($id != null)
+        {
+            LibraryStaff::destroy($id);
+        }
+
+        return redirect('admins/manage/libraryStaffs');
     }
 
     /**
      * Show the adminStaff registration form
+     * and currently registered adminStaff
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function manageAdminStaff()
+    public function manageAdminStaffs()
     {
-        return view('adminStaff.auth.register');
+        $adminStaffArr = AdminStaff::all();
+        return view('adminStaff.auth.register', ['adminStaffs' => $adminStaffArr, 'count' => 0]);
+    }
+
+    /**
+     * Remove an adminStaff
+     *
+     * @param $id
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
+    public function removeAdminStaff($id)
+    {
+        if ($id != null)
+        {
+            AdminStaff::destroy($id);
+        }
+
+        return redirect('admins/manage/adminStaffs');
     }
 
     /**
      * Show the hostelStaff registration form
+     * and currently registered hostelStaff
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function manageHostelStaff()
+    public function manageHostelStaffs()
     {
-        return view('hostelStaff.auth.register');
+        $hostelStaffArr = HostelStaff::all();
+        return view('hostelStaff.auth.register', ['hostelStaffs' => $hostelStaffArr, 'count' => 0]);
+    }
+
+    /**
+     * Remove a hostelStaff
+     *
+     * @param $id
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
+    public function removeHosteltaff($id)
+    {
+        if ($id != null)
+        {
+            HostelStaff::destroy($id);
+        }
+
+        return redirect('admins/manage/hostelStaffs');
+    }
+
+    /**
+     * Show the currently registered students
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function manageStudents()
+    {
+        $studentArr = Student::all();
+        return view('admin.manage.students', ['students' => $studentArr, 'count' => 0]);
+    }
+
+    /**
+     * Remove a student
+     *
+     * @param $rollNo
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
+    public function removeStudent($rollNo)
+    {
+        if ($rollNo != null)
+        {
+            Student::destroy($rollNo);
+        }
+
+        return redirect('admins/manage/students');
     }
 }
+
