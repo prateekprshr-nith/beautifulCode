394ca8aa0ae0c26ed65eca8807fc1a3e06c6d566
Prateek Prasher <prateekprshr@gmail.com>
2016-03-12 11:17:27 +0530

    Added home middleware to all auth routes

--- semesterRegistration/app/Http/routes.php
+++ semesterRegistration/app/Http/routes.php
@@ -8,23 +8,22 @@
 Route::group(['middleware' => 'web'], function () {
 
     // Landing page
-    // #TODO fix for other users
     Route::get('/', function () {
         return view('welcome');
-    })->middleware('guest:student');
+    })->middleware('home');
 
     // Student route group
     Route::group(['prefix' => '/students'], function () {
 
         //Student auth routes
-        Route::get('login', 'Student\Auth\AuthController@showLoginForm');
+        Route::get('login', 'Student\Auth\AuthController@showLoginForm')->middleware('home');
         Route::post('login', 'Student\Auth\AuthController@login');
         Route::get('logout', 'Student\Auth\AuthController@logout');
         Route::post('password/email', 'Student\Auth\PasswordController@sendResetLinkEmail');
         Route::post('password/reset', 'Student\Auth\PasswordController@reset');
-        Route::get('password/reset/{token?}', 'Student\Auth\PasswordController@showResetForm');
+        Route::get('password/reset/{token?}', 'Student\Auth\PasswordController@showResetForm')->middleware('home');
         Route::post('register', 'Student\Auth\AuthController@register');
-        Route::get('register', 'Student\Auth\AuthController@showRegistrationForm');
+        Route::get('register', 'Student\Auth\AuthController@showRegistrationForm')->middleware('home');
 
         // Student view routes
         Route::get('home', 'Student\HomeController@index');
@@ -34,12 +33,12 @@ Route::group(['middleware' => 'web'], function () {
     Route::group(['prefix' => '/teachers'], function () {
 
         //Teacher auth routes
-        Route::get('login', 'Teacher\Auth\AuthController@showLoginForm');
+        Route::get('login', 'Teacher\Auth\AuthController@showLoginForm')->middleware('home');
         Route::post('login', 'Teacher\Auth\AuthController@login');
         Route::get('logout', 'Teacher\Auth\AuthController@logout');
         Route::post('password/email', 'Teacher\Auth\PasswordController@sendResetLinkEmail');
         Route::post('password/reset', 'Teacher\Auth\PasswordController@reset');
-        Route::get('password/reset/{token?}', 'Teacher\Auth\PasswordController@showResetForm');
+        Route::get('password/reset/{token?}', 'Teacher\Auth\PasswordController@showResetForm')->middleware('home');;
         Route::post('register', 'Teacher\Auth\AuthController@register');
 
         // Manual registration is disabled
@@ -53,12 +52,12 @@ Route::group(['middleware' => 'web'], function () {
     Route::group(['prefix' => '/libraryStaffs'], function () {
 
         //LibraryStaff auth routes
-        Route::get('login', 'LibraryStaff\Auth\AuthController@showLoginForm');
+        Route::get('login', 'LibraryStaff\Auth\AuthController@showLoginForm')->middleware('home');
         Route::post('login', 'LibraryStaff\Auth\AuthController@login');
         Route::get('logout', 'LibraryStaff\Auth\AuthController@logout');
         Route::post('password/email', 'LibraryStaff\Auth\PasswordController@sendResetLinkEmail');
         Route::post('password/reset', 'LibraryStaff\Auth\PasswordController@reset');
-        Route::get('password/reset/{token?}', 'LibraryStaff\Auth\PasswordController@showResetForm');
+        Route::get('password/reset/{token?}', 'LibraryStaff\Auth\PasswordController@showResetForm')->middleware('home');
         Route::post('register', 'LibraryStaff\Auth\AuthController@register');
 
         // Manual registration is disabled
@@ -72,12 +71,12 @@ Route::group(['middleware' => 'web'], function () {
     Route::group(['prefix' => '/hostelStaffs'], function () {
 
         //HostelStaff auth routes
-        Route::get('login', 'HostelStaff\Auth\AuthController@showLoginForm');
+        Route::get('login', 'HostelStaff\Auth\AuthController@showLoginForm')->middleware('home');
         Route::post('login', 'HostelStaff\Auth\AuthController@login');
         Route::get('logout', 'HostelStaff\Auth\AuthController@logout');
         Route::post('password/email', 'HostelStaff\Auth\PasswordController@sendResetLinkEmail');
         Route::post('password/reset', 'HostelStaff\Auth\PasswordController@reset');
-        Route::get('password/reset/{token?}', 'HostelStaff\Auth\PasswordController@showResetForm');
+        Route::get('password/reset/{token?}', 'HostelStaff\Auth\PasswordController@showResetForm')->middleware('home');
         Route::post('register', 'HostelStaff\Auth\AuthController@register');
 
         // Manual registration is disabled
@@ -91,12 +90,12 @@ Route::group(['middleware' => 'web'], function () {
     Route::group(['prefix' => '/adminStaffs'], function () {
 
         //AdminStaff auth routes
-        Route::get('login', 'AdminStaff\Auth\AuthController@showLoginForm');
+        Route::get('login', 'AdminStaff\Auth\AuthController@showLoginForm')->middleware('home');
         Route::post('login', 'AdminStaff\Auth\AuthController@login');
         Route::get('logout', 'AdminStaff\Auth\AuthController@logout');
         Route::post('password/email', 'AdminStaff\Auth\PasswordController@sendResetLinkEmail');
         Route::post('password/reset', 'AdminStaff\Auth\PasswordController@reset');
-        Route::get('password/reset/{token?}', 'AdminStaff\Auth\PasswordController@showResetForm');
+        Route::get('password/reset/{token?}', 'AdminStaff\Auth\PasswordController@showResetForm')->middleware('home');
         Route::post('register', 'AdminStaff\Auth\AuthController@register');
 
         // Manual registration is disabled
@@ -110,7 +109,7 @@ Route::group(['middleware' => 'web'], function () {
     Route::group(['prefix' => '/admins'], function () {
 
         //Admin auth routes
-        Route::get('login', 'Admin\Auth\AuthController@showLoginForm');
+        Route::get('login', 'Admin\Auth\AuthController@showLoginForm')->middleware(['home']);
         Route::post('login', 'Admin\Auth\AuthController@login');
         Route::get('logout', 'Admin\Auth\AuthController@logout');
 
