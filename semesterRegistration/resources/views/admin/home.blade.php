b4d94c7618a3b3b34e7b8eaf0acf969b21f5aa9b
Prateek Prasher <prateekprshr@gmail.com>
2016-03-12 22:48:11 +0530

    Added user management panel

--- semesterRegistration/resources/views/admin/home.blade.php
+++ semesterRegistration/resources/views/admin/home.blade.php
@@ -3,15 +3,32 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading"><strong>Manage users</strong></div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <div class="btn-group btn-group-justified">
+                        <a href="/admins/manage/teachers" class="btn btn-primary">
+                            <span class="glyphicon glyphicon-user"></span> Manage Teachers
+                        </a>
+                        <a href="/admins/manage/hostelStaffs" class="btn btn-default">
+                            <span class="glyphicon glyphicon-user"></span> Manage Hostel Staff
+                        </a>
+                        <a href="/admins/manage/adminStaffs" class="btn btn-primary">
+                            <span class="glyphicon glyphicon-user"></span> Manage Admin Staff
+                        </a>
+                        <a href="/admins/manage/libraryStaffs" class="btn btn-default">
+                            <span class="glyphicon glyphicon-user"></span> Manage Library Staff
+                        </a>
+                        <a href="/admins/manage/students" class="btn btn-primary">
+                            <span class="glyphicon glyphicon-user"></span> Manage Students
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 </div>
 @endsection
