0a451395c398718501d6ef15123e7049d2079188
Prateek Prasher <prateekprshr@gmail.com>
2016-03-12 22:53:17 +0530

    Added student management view

---
+++ semesterRegistration/resources/views/admin/manage/students.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <strong>Students currently registered in the system</strong>
+                    </div>
+                        <div class="panel-body">
+                            @if (count($students) > 0)
+                                <!-- Current students list -->
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Roll No</th>
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                            <th>Deparment Code</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach ($students as $student)
+                                        <tr>
+                                            <td>{{ ++$count }}</td>
+                                            <td>{{ $student->rollNo }}</td>
+                                            <td>{{ $student->name }}</td>
+                                            <td>{{ $student->email }}</td>
+                                            <td>{{ $student->dCode}}</td>
+                                            <td>
+                                                <form action="/admins/manage/students/{{$student->rollNo}}" method="POST">
+                                                    {{ csrf_field() }}
+                                                    {{ method_field('DELETE') }}
+
+                                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">
+                                                        <span class="glyphicon glyphicon-remove"></span> Remove
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            @else
+                                No students are currently registered in the system.
+                            @endif
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
