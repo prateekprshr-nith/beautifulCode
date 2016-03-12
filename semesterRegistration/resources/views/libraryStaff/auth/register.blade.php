bc4cec7cb010ec0f488b307f993b0fc19f08fc26
Prateek Prasher <prateekprshr@gmail.com>
2016-03-12 23:55:42 +0530

    Changed @extends and @section

--- semesterRegistration/resources/views/libraryStaff/auth/register.blade.php
+++ semesterRegistration/resources/views/libraryStaff/auth/register.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.app')
+@extends('admin.manage.libraryStaffs')
 
-@section('content')
+@section('libraryStaffRegPanel')
     <div class="container col-sm-offset-2 col-sm-8">
         <div class="panel panel-default">
             <div class="panel-heading">
-                Registration: Please enter your details correctly
+                <strong>Add a new library staff member</strong>
             </div>
             <div class="panel-body">
                 <form class="form-horizontal" role="form" method="POST" action="/libraryStaffs/register"
