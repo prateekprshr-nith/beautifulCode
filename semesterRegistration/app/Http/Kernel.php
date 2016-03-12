36025108eb85b27f49d641eb9950de90f433c9cb
Prateek Prasher <prateekprshr@gmail.com>
2016-03-12 11:41:19 +0530

    Added RedirectIfNotAdminIp middleware's key 'adminIp'

--- semesterRegistration/app/Http/Kernel.php
+++ semesterRegistration/app/Http/Kernel.php
@@ -49,5 +49,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'home' => \App\Http\Middleware\RedirectToHome::class,
+        'adminIp' => \App\Http\Middleware\RedirectIfNotAdminIp::class,
     ];
 }
