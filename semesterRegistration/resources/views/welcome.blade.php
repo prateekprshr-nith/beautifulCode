9df4002676040b0c065915f33fa05d829bbbe0b4
Prateek Prasher <prateekprshr@gmail.com>
2016-03-14 00:53:47 +0530

    Added vertical alignment to the NIT Image

--- semesterRegistration/resources/views/welcome.blade.php
+++ semesterRegistration/resources/views/welcome.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<style>
+    .element {
+        position: relative;
+        top: 50%;
+        transform: translateY(10%);
+    }
+</style>
+<div class="container element">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
