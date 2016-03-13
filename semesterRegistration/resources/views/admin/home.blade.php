909208cb557c84fe430fc988b317037685afc2e4
Prateek Prasher <prateekprshr@gmail.com>
2016-03-13 21:40:53 +0530

    Added department and section management panel

--- semesterRegistration/resources/views/admin/home.blade.php
+++ semesterRegistration/resources/views/admin/home.blade.php
@@ -2,33 +2,52 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading"><strong>Manage users</strong></div>
 
-                <div class="panel-body">
-                    <div class="btn-group btn-group-justified">
-                        <a href="/admins/manage/teachers" class="btn btn-primary">
-                            <span class="glyphicon glyphicon-user"></span> Manage Teachers
-                        </a>
-                        <a href="/admins/manage/hostelStaffs" class="btn btn-default">
-                            <span class="glyphicon glyphicon-user"></span> Manage Hostel Staff
-                        </a>
-                        <a href="/admins/manage/adminStaffs" class="btn btn-primary">
-                            <span class="glyphicon glyphicon-user"></span> Manage Admin Staff
-                        </a>
-                        <a href="/admins/manage/libraryStaffs" class="btn btn-default">
-                            <span class="glyphicon glyphicon-user"></span> Manage Library Staff
-                        </a>
-                        <a href="/admins/manage/students" class="btn btn-primary">
-                            <span class="glyphicon glyphicon-user"></span> Manage Students
-                        </a>
-                    </div>
+    <!-- First panel: Manage users -->
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <strong>Manage users</strong>
+            </div>
+            <div class="panel-body">
+                <div class="btn-group btn-group-justified">
+                    <a href="/admins/manage/teachers" class="btn btn-primary">
+                        <span class="glyphicon glyphicon-user"></span> Manage Teachers
+                    </a>
+                    <a href="/admins/manage/hostelStaffs" class="btn btn-default">
+                        <span class="glyphicon glyphicon-user"></span> Manage Hostel Staff
+                    </a>
+                    <a href="/admins/manage/adminStaffs" class="btn btn-primary">
+                        <span class="glyphicon glyphicon-user"></span> Manage Admin Staff
+                    </a>
+                    <a href="/admins/manage/libraryStaffs" class="btn btn-default">
+                        <span class="glyphicon glyphicon-user"></span> Manage Library Staff
+                    </a>
+                    <a href="/admins/manage/students" class="btn btn-primary">
+                        <span class="glyphicon glyphicon-user"></span> Manage Students
+                    </a>
                 </div>
             </div>
         </div>
+    </div>
 
+    <!-- Second panle: Manage departments and sections -->
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <strong>Manage departments and sections</strong>
+            </div>
+            <div class="panel-body">
+                <div class="btn-group btn-group-justified">
+                    <a href="/admins/manage/departments" class="btn btn-primary">
+                        <span class="glyphicon glyphicon-user"></span> Manage Departments
+                    </a>
+                    <a href="/admins/manage/sections" class="btn btn-default">
+                        <span class="glyphicon glyphicon-user"></span> Manage Sections
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
