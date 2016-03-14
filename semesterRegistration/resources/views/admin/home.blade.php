ff837d1d54c694783e7236e2a3bada123957e8e8
Prateek Prasher <prateekprshr@gmail.com>
2016-03-14 10:59:50 +0530

    Made all the button classes default

--- semesterRegistration/resources/views/admin/home.blade.php
+++ semesterRegistration/resources/views/admin/home.blade.php
@@ -12,19 +12,19 @@
             </div>
             <div class="panel-body">
                 <div class="btn-group btn-group-justified">
-                    <a href="/admins/manage/teachers" class="btn btn-primary">
+                    <a href="/admins/manage/teachers" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Manage Teachers
                     </a>
                     <a href="/admins/manage/hostelStaffs" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Manage Hostel Staff
                     </a>
-                    <a href="/admins/manage/adminStaffs" class="btn btn-primary">
+                    <a href="/admins/manage/adminStaffs" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Manage Admin Staff
                     </a>
                     <a href="/admins/manage/libraryStaffs" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Manage Library Staff
                     </a>
-                    <a href="/admins/manage/students" class="btn btn-primary">
+                    <a href="/admins/manage/students" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Manage Students
                     </a>
                 </div>
@@ -41,13 +41,13 @@
             </div>
             <div class="panel-body">
                 <div class="btn-group btn-group-justified">
-                    <a href="/admins/manage/departments" class="btn btn-primary">
+                    <a href="/admins/manage/departments" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Manage Departments
                     </a>
                     <a href="/admins/manage/sections" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Manage Sections
                     </a>
-                    <a href="/admins/manage/hostels" class="btn btn-primary">
+                    <a href="/admins/manage/hostels" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Manage Hostels
                     </a>
                 </div>
