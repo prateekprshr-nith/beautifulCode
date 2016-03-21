7429a3e595a62a492622821262a8f9a369bed0f7
Prateek Prasher <prateekprshr@gmail.com>
2016-03-22 00:16:05 +0530

    Added getImage()

--- semesterRegistration/app/Http/Controllers/Student/HomeController.php
+++ semesterRegistration/app/Http/Controllers/Student/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Student;
 
+use App\StudentImage;
 use App\Http\Requests;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Intervention\Image\Facades\Image;
 
 /**
  * Class HomeController, this class contains
@@ -25,6 +27,7 @@ class HomeController extends Controller
     {
         $this->middleware('auth:student');
         $this->middleware('verify:student');
+        $this->middleware('noImage');
     }
 
     /**
@@ -36,4 +39,31 @@ class HomeController extends Controller
     {
         return view('student.home');
     }
+
+    /**
+     * Return the image of the student.
+     *
+     * @return mixed
+     */
+    public function getImage ()
+    {
+        $rollNo = Auth::guard('student')->user()->rollNo;
+
+        // Get the student image and return
+        // it as http response
+        $imageEntry = StudentImage::find($rollNo);
+        
+        if($imageEntry != null)
+        {
+            $imagePath = $imageEntry->imagePath;
+        }
+        else
+        {
+            $imagePath = env('IMAGE_DIR') . 'circle.png';
+        }
+
+        $image = Image::make($imagePath);
+
+        return $image->response();
+    } 
 }
