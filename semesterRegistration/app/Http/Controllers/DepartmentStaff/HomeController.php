a230065c7b8f117e3de24eabadf9e5f2ff087c05
Prateek Prasher <prateekprshr@gmail.com>
2016-04-09 14:14:56 +0530

    Added course management functions

--- semesterRegistration/app/Http/Controllers/DepartmentStaff/HomeController.php
+++ semesterRegistration/app/Http/Controllers/DepartmentStaff/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\DepartmentStaff;
 
+use App\Course;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Class HomeController, this class contains
@@ -34,6 +36,56 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('departmentStaff.home');
+        // Get the list of courses
+        $courses = Course::where('dCode', Auth::guard('departmentStaff')->user()->dCode)->get();
+
+        // Department code
+        $dCode = Auth::guard('departmentStaff')->user()->dCode;
+
+        return view('departmentStaff.home', ['courses' => $courses, 'count' => 0, 'dCode' => $dCode]);
+    }
+
+    /**
+     * Add a new course
+     *
+     * @param Request $request
+     * @return mixed
+     */
+    public function addCourse (Request $request)
+    {
+        $this->validate($request, [
+            'courseCode' => 'required|unique:courses',
+            'courseName' => 'required',
+        ], [
+            'unique' => 'This course is already present in the database'
+        ]);
+
+        $course = [
+            'courseCode' => $request['courseCode'],
+            'dCode' => Auth::guard('departmentStaff')->user()->dCode,
+            'courseName' => $request['courseName'],
+        ];
+
+        // Save the course
+        Course::create($course);
+
+        return redirect()->back()
+            ->with('status', 'Success');
+    }
+
+    /**
+     * Remove a course
+     *
+     * @param Request $request
+     * @return mixed
+     */
+    public function removeCourse (Request $request)
+    {
+        $courseCode = $request['courseCode'];
+
+        // Remove the course
+        Course::destroy($courseCode);
+
+        return redirect()->back();
     }
 }
