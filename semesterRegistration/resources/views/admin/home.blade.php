546aba85a77c92ba1f793f2a703e5ddc672b8453
Prateek Prasher <prateekprshr@gmail.com>
2016-04-11 13:01:25 +0530

    Added registration status toggling panel

--- semesterRegistration/resources/views/admin/home.blade.php
+++ semesterRegistration/resources/views/admin/home.blade.php
@@ -22,13 +22,13 @@
                         <span class="glyphicon glyphicon-user"></span> Department Staff
                     </a>
                     <a href="/admins/manage/adminStaffs" class="btn btn-default">
-                        <span class="glyphicon glyphicon-user"></span> Admin Staff
+                        <span class="glyphicon glyphicon-user"></span> Accounts Staff
                     </a>
                     <a href="/admins/manage/libraryStaffs" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Library Staff
                     </a>
                     <a href="/admins/manage/chiefWardenStaffs" class="btn btn-default">
-                        <span class="glyphicon glyphicon-user"></span> ChiefWarden Staff
+                        <span class="glyphicon glyphicon-user"></span> Chief Warden Staff
                     </a>
                     <a href="/admins/manage/students" class="btn btn-default">
                         <span class="glyphicon glyphicon-user"></span> Students
@@ -48,15 +48,79 @@
             <div class="panel-body">
                 <div class="btn-group btn-group-justified">
                     <a href="/admins/manage/departments" class="btn btn-default">
-                        <span class="glyphicon glyphicon-user"></span> Manage Departments
+                        <span class="glyphicon glyphicon-user"></span> Departments
                     </a>
                     <a href="/admins/manage/sections" class="btn btn-default">
-                        <span class="glyphicon glyphicon-user"></span> Manage Sections
+                        <span class="glyphicon glyphicon-user"></span> Sections
                     </a>
                     <a href="/admins/manage/hostels" class="btn btn-default">
-                        <span class="glyphicon glyphicon-user"></span> Manage Hostels
+                        <span class="glyphicon glyphicon-user"></span> Hostels
                     </a>
                 </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 col-md-offset-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <span class="glyphicon glyphicon-dashboard"></span>
+                <strong> Manage Semester Registration Process</strong>
+            </div>
+            <div class="panel-body">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Users</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Staff</td>
+
+                            @if($staffRegistrationStatus == 'Activated')
+                                <td class="text-info">{{$staffRegistrationStatus}}</td>
+                            @else
+                                <td class="text-muted">{{$staffRegistrationStatus}}</td>
+                            @endif
+
+                            <td>
+                                @if($staffRegistrationStatus == 'Activated')
+                                    <a href="/admins/toggleRegistrationProcess/staff" class="btn btn-sm btn-danger">
+                                        <span class="glyphicon glyphicon-remove"></span> De-Activate
+                                    </a>
+                                @else
+                                    <a href="/admins/toggleRegistrationProcess/staff" class="btn btn-sm btn-primary">
+                                        <span class="glyphicon glyphicon-ok"></span> Activate
+                                    </a>
+                                @endif
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Students</td>
+
+                            @if($studentRegistrationStatus == 'Activated')
+                                <td class="text-info">{{$studentRegistrationStatus}}</td>
+                            @else
+                                <td class="text-muted">{{$studentRegistrationStatus}}</td>
+                            @endif
+
+                            <td>
+                                @if($studentRegistrationStatus == 'Activated')
+                                    <a href="/admins/toggleRegistrationProcess/students" class="btn btn-sm btn-danger">
+                                        <span class="glyphicon glyphicon-remove"></span> De-Activate
+                                    </a>
+                                @else
+                                    <a href="/admins/toggleRegistrationProcess/students" class="btn btn-sm btn-primary">
+                                        <span class="glyphicon glyphicon-ok"></span> Activate
+                                    </a>
+                                @endif
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
