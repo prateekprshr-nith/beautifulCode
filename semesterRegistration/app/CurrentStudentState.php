a56cc07e8d0f56fbecaa5147c81ee95e1917751f
Prateek Prasher <prateekprshr@gmail.com>
2016-04-12 22:27:26 +0530

    Added CurrentStudentState model

---
+++ semesterRegistration/app/CurrentStudentState.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class CurrentStudentState, this model corresponds
+ * to createStudentStates database table
+ *
+ * @package App
+ */
+class CurrentStudentState extends Model
+{
+    protected $table = 'currentStudentStates';
+    protected $primaryKey = 'rollNo';
+    public $incrementing = false;
+
+    // Fillable and hidden arrtibutes
+    protected $fillable = [
+        'rollNo', 'hostler', 'feeReceipt',
+    ];
+
+    /**
+     * Get the student associated with the record
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function student ()
+    {
+        return $this->belongsTo('App\Student', 'rollNo', 'rollNo');
+    }
+}
