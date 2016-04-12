286550ad9698f2a97bf0a4c9b6134b578fb2aa78
Prateek Prasher <prateekprshr@gmail.com>
2016-04-12 22:27:10 +0530

    Added currentStudentStates database table

---
+++ semesterRegistration/database/migrations/2016_04_12_164535_create_current_student_states_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateCurrentStudentStatesTable, this class
+ * creates the currentStudentStates database table
+ */
+class CreateCurrentStudentStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currentStudentStates', function (Blueprint $table)
+        {
+            $table->string('rollNo', '20');
+            $table->boolean('hostler');
+            $table->boolean('feeReceipt');
+            $table->timestamps();
+
+            // Key constraints
+            $table->primary('rollNo');
+            $table->foreign('rollNo')
+                ->references('rollNo')
+                ->on('students')
+                ->onDelete('cascade')
+                ->onUpdate('cascade'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('currentStudentStates');
+    }
+}
