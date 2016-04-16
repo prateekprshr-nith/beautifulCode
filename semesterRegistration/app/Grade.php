aa14230acb7eb0e1cfab9c5cdd186e88f68f409b
Prateek Prasher <prateekprshr@gmail.com>
2016-04-16 11:34:39 +0530

    Modified fillable attributes

--- semesterRegistration/app/Grade.php
+++ semesterRegistration/app/Grade.php
@@ -18,10 +18,8 @@ class Grade extends Model
 
     // Fillable and hidden attributes
     protected $fillable = [
-        'rollNo', 'sem1', 'sem2', 'sem3', 'sem4', 'sem5',
-        'sem6', 'sem7', 'sem8', 'sem9', 'sem10', 'sgpi',
+        'rollNo','semNo', 'sgpi', 'cgpi', 'supplementaries',
     ];
-
 
     /**
      * Model relationships
