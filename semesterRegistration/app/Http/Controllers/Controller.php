81ff48a8a1437d0f644f1fab71ebdc3a883e6f05
Prateek Prasher <prateekprshr@gmail.com>
2016-04-17 14:41:26 +0530

    Added isRegistrationActive()

--- semesterRegistration/app/Http/Controllers/Controller.php
+++ semesterRegistration/app/Http/Controllers/Controller.php
@@ -10,4 +10,35 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    // Common views for all users
+    protected $inactiveView = 'common.inactive';
+
+    /**
+     * This function checks if the registration
+     * is active for a particular type of user
+     *
+     * @param $user
+     * @return bool
+     */
+    protected function isRegistrationActive ($user)
+    {
+        if($user === 'student')
+        {
+            $filename = storage_path() . '/app/activeForStudents';
+        }
+        else
+        {
+            $filename = storage_path() . '/app/activeForStaff';
+        }
+        
+        if(file_exists($filename))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
