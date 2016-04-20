2e0ce09f5656cf0c63a6ccb672cd436e40088292
Prateek Prasher <prateekprshr@gmail.com>
2016-04-20 08:19:00 +0530

    Added adminStaffRequests table

---
+++ semesterRegistration/database/migrations/2016_04_17_103629_create_adminStaffRequests_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateAdminStaffRequestsTable, this class
+ * creates the adminStaffRequests database table
+ */
+class CreateAdminStaffRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adminStaffRequests', function (Blueprint $table)
+        {
+            $table->string('rollNo', '20');
+            $table->string('status', '20');
+            $table->string('remarks', '200') ->nullable();
+            $table->timestamps();
+
+            // Key constraints
+            $table->primary('rollNo');
+            $table->foreign('rollNo')
+                ->references('rollNo')
+                ->on('students')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('adminStaffRequests');
+    }
+}
