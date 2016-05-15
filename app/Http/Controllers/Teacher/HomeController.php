4b4e34596abc9205f8deb37d70016f51e52f4ee1
Prateek Prasher <prateekprshr@gmail.com>
2016-05-16 00:54:23 +0530

    Added studentElectiveList management functions

--- app/Http/Controllers/Teacher/HomeController.php
+++ app/Http/Controllers/Teacher/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Teacher;
 
+use App;
+use App\Course;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Class HomeController, this class contains
@@ -17,6 +20,7 @@ use App\Http\Controllers\Controller;
 class HomeController extends Controller
 {
     protected $helpView = 'teacher.help';
+    protected $electiveSelectionView = 'teacher.electives';
 
     /**
      * Create a new controller instance.
@@ -37,5 +41,48 @@ class HomeController extends Controller
     public function index()
     {
         return view('teacher.home');
+    }
+
+    public function showElectiveSelectionView()
+    {
+        // Get electives
+        $electives = Course::where('dCode', Auth::guard('teacher')->user()->dCode)
+            ->where(function($q) {
+                $q->where('openElective', true)->orWhere('departmentElective', true);
+            })->get();
+
+        return view($this->electiveSelectionView, ['electives' => $electives]);
+    }
+
+    public function getElectiveList ($courseCode)
+    {
+        // Get the list of allocated electives
+        $allocatedElectives = Course::find($courseCode)->allocatedElectives;
+
+        if($allocatedElectives->isEmpty())
+        {
+            $studentList = 'No student has opted for this subject !!!';
+        }
+        else
+        {
+            $studentList = '';
+            $count = 1;
+
+            // Generate the student list
+            foreach ($allocatedElectives as $allocatedElective)
+            {
+                $studentList = $studentList . "\n" . $count . ") " .
+                    $allocatedElective->rollNo . ", " .
+                    $allocatedElective->currentStudentState->student->name  . ", " .
+                    $allocatedElective->currentStudentState->student->department->dName;
+            }
+        }
+
+        $list = App::make('snappy.pdf.wrapper');
+        $list->loadHTML($studentList)
+            ->setOption('margin-bottom', 10)
+            ->setOption('margin-top', 10);
+
+        return $list->download($courseCode . 'List.pdf');
     }
 }
